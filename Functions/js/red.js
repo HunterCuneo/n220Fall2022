e4ff64c9ccfaab21e01dd8bfedec0f3d68e51738
Hunter Cuneo <hucuneo@iu.edu>
2022-09-15 23:15:30 -0400

    Update red.js

--- Functions/js/red.js
+++ Functions/js/red.js
@@ -3,16 +3,16 @@ noRed = color(170,200,150)
 function setup(){
     createCanvas (800,600);
     background (0);
-    Redcolor = fill(170,200,150);
-    noRed = fill(0,200,150);
+    Redcolor = fill(170,200,150); //Sets the color to one that contains red
+    noRed = fill(0,200,150); //sets the color to one that does not contain red
 }
 
 function ball() {
-    circle(300, 300, 45);
-    Redcolor;
+    circle(300, 300, 45); //circle created for testing color
+    Redcolor; //sets default ball color to include red
 }
 
 function draw (x,y) {
-    ball();
-    noRed;
+    ball(); //creates ball on canvas
+    noRed; //changes color to one that does not contain red
 }
